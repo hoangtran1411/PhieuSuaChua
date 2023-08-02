9613f8aae4a53a8bcc9293107c1207940b293913
Administrator <Administrator@HOANG-HOME>
2023-08-02 22:02:44 +0700

    done

--- Phieu/Controllers/TraCuuPhieuController.cs
+++ Phieu/Controllers/TraCuuPhieuController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Phieu.Models;
 using PhieuSuaChua.Domain_Model;
 using PhieuSuaChua.Models;
@@ -15,21 +16,7 @@ namespace PhieuSuaChua.Controllers
 
         public IActionResult Index()
         {
-            //var listPhieu = (from ChiTietPhieu in context.ChiTietPhieus
-            //                 join Phieu in context.Phieus
-            //                 on ChiTietPhieu.ID equals Phieu.IDchiTiet
-            //                 join NhanVien in context.NhanViens
-            //                 on ChiTietPhieu.MaNv equals NhanVien.MaNv
-            //                 select new ModelTraCuuPhieu
-            //                 {
-            //                     Id = Phieu.ID,
-            //                     Ngaytaophieu = Phieu.NgayTaoPhieu,
-            //                     Hoten = NhanVien.TenNv,
-            //                     Donvi = NhanVien.DonVi,
-            //                     Maytinh = ChiTietPhieu.TenMayTinh,
-            //                     Thietbikhac = ChiTietPhieu.ThietBiKhac,
-            //                     Trangthaiphieu = ChiTietPhieu.TrangThaiPhieu
-            //                 }).ToList();
+         
             var listPhieu = (from ChiTietPhieu in context.Chitietsuas
                              join Phieusua in context.Phieusuas
                              on ChiTietPhieu.IdPhieu equals Phieusua.IdPhieu
@@ -45,8 +32,8 @@ namespace PhieuSuaChua.Controllers
                                  Thietbikhac = ChiTietPhieu.ThietBiKhac,
                                  Trangthaiphieu = Phieusua.TrangThaiPhieu
                              }).ToList();
-            //test
-                            
+          var lits =  context.Database.ExecuteSqlAsync($"dbo.InfoPhieu");
+
             return View(listPhieu);
         }
     }
